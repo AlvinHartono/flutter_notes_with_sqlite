ae247d19a0ffe1bc67be48949174b39513566dcb
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2023-11-19 21:41:19 +0700

    added listview builder

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projext/SQLite/db_helper.dart';
+import 'package:projext/models/note.dart';
 import 'package:projext/screens/create_notes.dart';
 
 class Notes extends StatefulWidget {
@@ -9,7 +11,34 @@ class Notes extends StatefulWidget {
 }
 
 class _NotesState extends State<Notes> {
+  late DatabaseHelper handler;
+  late Future<List<Note>> notes;
+  final db = DatabaseHelper();
+
+  final title = TextEditingController();
+  final content = TextEditingController();
+  final keyword = TextEditingController();
+
   final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    handler = DatabaseHelper();
+    notes = handler.getNotes();
+    handler.initDB().whenComplete(() => notes = getAllNotes());
+    super.initState();
+  }
+
+  Future<List<Note>> getAllNotes() {
+    return handler.getNotes();
+  }
+
+  Future<void> _refresh() async {
+    setState(() {
+      notes = getAllNotes();
+    });
+  }
 
   @override
   void dispose() {
@@ -29,19 +58,80 @@ class _NotesState extends State<Notes> {
             end: Alignment.bottomRight,
           )),
         ),
-        title: TextField(
-          controller: _searchController,
-          style: const TextStyle(color: Colors.white),
-          cursorColor: Colors.white,
-          decoration: const InputDecoration(
-            hintText: 'Search...',
-            hintStyle: TextStyle(color: Colors.white54),
-            border: InputBorder.none,
+        title: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          margin: const EdgeInsets.symmetric(horizontal: 8),
+          child: TextField(
+            controller: _searchController,
+            style: const TextStyle(color: Colors.white),
+            cursorColor: Colors.white,
+            decoration: const InputDecoration(
+              hintText: 'Search...',
+              hintStyle: TextStyle(color: Colors.white54),
+              border: InputBorder.none,
+            ),
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                //call a function to do some searchin'
+              }
+              if (value.isEmpty) {
+                notes = getAllNotes();
+              }
+            },
           ),
         ),
       ),
-      body: const Center(
-        child: Text("data"),
+      body: Expanded(
+        child: FutureBuilder<List<Note>>(
+          future: notes,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData && snapshot.data!.isEmpty) {
+              return const Center(
+                child: Text('no data'),
+              );
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            } else {
+              final items = snapshot.data ?? <Note>[];
+              return ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(items[index].title),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        db
+                            .deleteNote(items[index].id!)
+                            .whenComplete(() => _refresh());
+                      },
+                    ),
+                    onTap: () {
+                      setState(() {
+                        // title.text = items[index].noteTitle;
+                        // content.text = items[index].noteContent;
+                      });
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            actions: [
+                              Row(
+                                children: [],
+                              )
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.of(context).push(
